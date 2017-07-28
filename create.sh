c1506bdfa618384cca7e47d4d515ded5531552fe
Michael Prather <mprather1@gmail.com>
2017-07-28 06:16:27 +0000

    0.0.2

---
+++ create.sh
@@ -0,0 +1,23 @@
+printf "
+#!/usr/bin/env bash
+
+chkarg(){
+if [ ! -z \${1+x} ]; then
+return 0
+else
+return 1
+fi
+}
+
+
+
+update_version(){
+if chkarg \$2; then
+/usr/local/bin/node $(pwd)${directory}/bin/update-version/index.js \$1 \$2
+else
+printf '\\nplease provide arguments...\\n\$filename \$version\\n\\n'
+fi 
+}
+
+update_version \$1 \$2
+" > $(pwd)${directory}/bin/update-version/script.sh
--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 const yaml = require('js-yaml')
 const fs = require('fs')
-
+var path = require('path')
 var filename = process.argv[2]
 var version = process.argv[3]
 
 if (filename !== undefined) {
   try {
-    var v = yaml.safeLoad(fs.readFileSync(filename, 'utf8'))
+    var v = yaml.safeLoad(fs.readFileSync(path.join(__dirname, filename), 'utf8'))
     v.version = version
     var y = yaml.safeDump(v, {
       'sortkeys': true
     })
+
     fs.stat('./package.json', (err, results) => {
-      if (err) throw new Error(err)
+      if (err !== null && err.path !== './package.json') {
+        console.log(err)
+      }
+
       if (results !== undefined) {
         console.log('package.json exists...\nskipping version update...\n')
         return false
